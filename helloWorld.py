5853168884fb91224b6258a207813049f316ee4f
metenis <110682343+metenis@users.noreply.github.com>
2025-04-23 14:13:24 +0200

    Add files via upload

---
+++ helloWorld.py
@@ -0,0 +1,4 @@
+# This is a comment - anything after the "#" symbol is ignored by the interpreter
+
+# This is the main part of the program
+print("Hello, World!")  # This line prints "Hello, World!" to the screen
